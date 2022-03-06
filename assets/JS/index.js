120b8833a1625bef8fda71646a304ded601e98c0
Gabarron <>
2022-03-06 13:59:35 +0000

    Add swap turns logic to JS

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,22 +1,48 @@
 const cellElements = document.getElementsByClassName("data-cell");
+const board = document.getElementsByClassName("data-board")[0];
+const turnMessage = document.getElementsByClassName("turn-message-text")[0];
 
-let isCircleTurn;
+let isCircleTurn = false;
+
+function placeMark(event, classToAdd) {
+
+    event.classList.add(classToAdd);
+
+}
+
+function swapTurns() {
+
+    isCircleTurn = !isCircleTurn;
+    board.classList.remove('x');
+    board.classList.remove('circle');
+
+    if (isCircleTurn) {
+        board.classList.add('circle');
+        turnMessage.textContent = "Circle Turn"
+    } else {
+        board.classList.add('circle');
+        turnMessage.textContent = "X Turn"
+    }
+
+}
 
 function handleClick(event) {
 
     // Adding X and Circle
     let classToAdd = isCircleTurn ? 'circle' : 'x';
      
-    event.target.classList.add(classToAdd);
+    placeMark(event.target, classToAdd);
     // Check if win
     // Check if draw
     // Swap turns
-
+    swapTurns();
 }
 
   for (const cell of cellElements) {
 
-    
+    cell.classList.remove("circle");
+    cell.classList.remove("x");
+    cell.removeEventListener("click", handleClick);
     cell.addEventListener("click", handleClick, { once: true });
 
   }
