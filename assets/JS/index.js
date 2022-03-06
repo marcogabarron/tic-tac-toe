9272de8348ff973045fa89baabf7652bd57b7568
Gabarron <>
2022-03-06 13:46:25 +0000

    Add handleClick function to JS file

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,16 +1,22 @@
 const cellElements = document.getElementsByClassName("data-cell");
 
+let isCircleTurn;
 
+function handleClick(event) {
+
+    // Adding X and Circle
+    let classToAdd = isCircleTurn ? 'circle' : 'x';
+     
+    event.target.classList.add(classToAdd);
+    // Check if win
+    // Check if draw
+    // Swap turns
+
+}
 
   for (const cell of cellElements) {
 
+    
     cell.addEventListener("click", handleClick, { once: true });
 
-  }
-
-  function handleClick() {
-      // Adding X and Circle
-      // Check if win
-      // Check if draw
-      // Swap turns
   }
